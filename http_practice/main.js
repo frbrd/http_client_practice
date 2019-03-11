503c72c0a99ec857ab7a175991449d0abf03b0e3
Melissa L <get.psychick@gmail.com>
2019-03-11 17:21:26 +0000

    calling the html through https client

---
+++ http_practice/main.js
@@ -0,0 +1,20 @@
+var https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, function (response) {
+      response.on('data', function (chunk) {
+        console.log('\n');
+        console.log(chunk.toString());
+      });
+})
+
+ 
+}
+
+ getAndPrintHTMLChunks();
